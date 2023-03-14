64a527bb94bac961bae153e97a1903c2418ee04b
richieli_i <richieli_i@didiglobal.com>
2023-03-14 20:28:49 +0800

    feat: layout-aside

--- L-chatClient/src/Pages/layout/index.tsx
+++ L-chatClient/src/Pages/layout/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import style from './index.module.less'
+import React from "react";
+import { Layout } from "antd";
+import style from "./index.module.less";
+import Component from "./components/index";
+const { Aside, HeaderTop, OperaMenu } = Component;
 
-export default function Layout() {
+export default function LayOut() {
   return (
-    <div className={style['layout-page']}>
-      
+    <div className={style["layout-page"]}>
+      <main className={style["chat-container"]}>
+          <div className={style["chat-message-aside"]}>
+            <Aside></Aside>
+          </div>
+          <div className={style["chat-message-content"]}>
+
+          </div>
+      </main>
     </div>
-  )
+  );
 }
---
+++ L-chatClient/src/utils/style.ts
@@ -0,0 +1,6 @@
+// const css = (name: string): string => {
+//   const classList: string[] = name.split(' ');
+//   return classList.map((item: string) => style[`${item}`]).join(' ');
+// };
+
+// export default css;
